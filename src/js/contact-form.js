3739e110cee032fcfa421453fdec6a268954a9fe
Julia Burkhart <juliaburkhart@julias-mbp.localdomain>
2021-02-14 11:24:38 +0100

    Kontaktformular funktioniert

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,3 +1,4 @@
+
 // const dropdownButton = document.querySelector(".dropdown-button");
 
 // dropdownButton.addEventListener("click", function () {
@@ -7,38 +8,76 @@
 //     .classList.toggle("show-dropdown-content");
 // });
 
+
+/// Variablen für Kontaktformular und Pflicht-Inputs
+
 const contactForm = document.querySelector('[name="contact-form"]');
+const nameInput = document.querySelector('[name="name"]');
+const emailInput = document.querySelector('[name="email"]');
+const privacyCheckbox = document.getElementById("privacy-check__checkbox");
+const privacyLabel = document.querySelector(".privacy-check__label");
+
+
+
+// Evtl. Fehlermeldungen zurücksetzen
+
+nameInput.addEventListener("blur", function () {
+  if (nameInput.value.length >= 2) {
+    nameInput.classList.remove("invalid-form");
+    nameInput.setCustomValidity("");
+  }
+});
+
+emailInput.addEventListener("blur", function () {
+  if (emailInput.value.length >= 5 || emailInput.value.includes("@")) {
+    emailInput.classList.remove("invalid-form");
+    emailInput.setCustomValidity("");
+  }
+});
+
+privacyCheckbox.addEventListener("change", function () {
+  if (privacyCheckbox.checked == true) {
+    privacyLabel.classList.remove("invalid-checkbox");
+  }
+})
+
+// Beim Senden den Inhalt des Formulars überprüfen
 
 contactForm.addEventListener("submit", function (event) {
+
   console.log("Form submitted");
+
+
+  /// Variablen für den aktuellen Inhalt der Inputs
 
   const customerName = event.currentTarget.name.value;
   const emailAdress = event.currentTarget.email.value;
-  const privacyCheckbox = document.getElementById("privacy-check__checkbox");
 
-  console.log(event.currentTarget);
+ 
 
   if (customerName.length < 2) {
     console.log("Name fehlt");
-    document.querySelector('[name="name"]').classList.add("invalid-form");
+    
+    nameInput.classList.add("invalid-form");
+    nameInput.setCustomValidity("Bitte trage hier noch deinen Namen ein!");
     event.preventDefault();
-  } else if (emailAdress.length < 2) {
+
+ 
+  } else if (emailAdress.length < 5 || !emailAdress.includes("@")) {
     console.log("Mailadresse fehlt");
-    document.querySelector('[name="email"]').classList.add("invalid-form");
+    
+    emailInput.classList.add("invalid-form");
+    emailInput.setCustomValidity("Bitte trage hier noch deine E-Mail Adresse ein!");
     event.preventDefault();
-  } else if (!emailAdress.includes("@")) {
-    console.log("Keine korrekte Mailadresse");
-    document.querySelector('[name="email"]').classList.add("invalid-form");
-    event.preventDefault();
+
   } else if (privacyCheckbox.checked == false) {
-    console.log("Checkbox nicht gecheckt");
-
-    document
-      .querySelector(".privacy-check__label")
-      .classList.add("invalid-checkbox");
-
+    console.log("Checkbox nicht gecheckt.");
+   
+    privacyLabel.classList.add("invalid-checkbox");
     event.preventDefault();
+
   } else {
-    console.log("Alles richtig");
+
+    alert("Alles richtig!");
   }
 });
